797b15513795aab6e48692a97a87bb8ecc9a9a0b
Cody Averett <codyaverett@gmail.com>
2024-09-13 23:31:46 -0500

    Add htmx

--- main.go
+++ main.go
@@ -1,23 +1,54 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
+func loadMorePosts(w http.ResponseWriter, r *http.Request) {
+	// Generate HTML for more blog posts
+	fmt.Fprint(w, `
+      <article>
+          <h3>New Post Title</h3>
+          <p>This is another dynamic blog post loaded with HTMX.</p>
+      </article>
+  `)
+}
+
+func submitComment(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	comment := r.FormValue("comment")
+
+	// Return the comment in HTML format
+	fmt.Fprintf(w, `
+      <li>%s</li>
+  `, comment)
+}
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hello, World")
+	fmt.Fprintf(w, "Hello, World")
+}
+
+func handleDefault(w http.ResponseWriter, r *http.Request) {
+	// Serve static files like index.html
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
 }
 
 func main() {
-  // Define routes
-  http.HandleFunc("/", helloHandler)
+	// Serve static files like index.html
+	fs := http.FileServer(http.Dir("./web"))
 
-  // Start the server
-  fmt.Println("Starting server at port 8080...")
-  if err := http.ListenAndServe(":8080", nil); err != nil {
-    log.Fatal(err)
-  }
+	// Define routes
+	http.Handle("/", fs)
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/load-more-posts", loadMorePosts)
+	http.HandleFunc("/submit-comment", submitComment)
+
+	// Start the server
+	fmt.Println("Starting server at port 8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
-
